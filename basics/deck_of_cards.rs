0fcd8d3b98d5158a0cf2d6948b75cb535e3c2bc1
Bithiah <bithiah.koshy@gmail.com>
2024-12-04 02:09:55 +1100

    Create deck_of_cards.rs

---
+++ basics/deck_of_cards.rs
@@ -0,0 +1,30 @@
+#[derive(Debug)]
+
+struct Deck {
+    cards: Vec<String>,
+}
+
+fn main() {
+    // List of suits' - 'heart', 'spades', 'clubs, 'diamonds'
+    // List of 'values' - 'ace', 'two', 'three', etc.
+
+    // double nested for loop
+
+    let suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
+    let values = ["Ace", "Two", "Three"];
+
+    let mut cards = vec![];
+
+    for suit in suits {
+        for value in values {
+            let card = format!("{} of {}", value, suit);
+            cards.push(card);
+        }
+    }
+
+    let deck = Deck { cards };
+
+    println!("Here's your deck: {:?}", deck);
+
+}
+
